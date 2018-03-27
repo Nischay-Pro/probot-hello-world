54a979b225f490ca7c5bd996bc68f61b1fb7b929
Nischay Pro <NischayPro@yahoo.co.in>
2018-03-27 20:18:33 +0530

    code: done

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+module.exports = (robot) =>  {
+  robot.on('pull_request.opened', receive); 
+  async function receive(context) {
+    const response = await context.github.issues.getForRepo(context.repo( {
+      state:'all', 
+      creator:context.payload.pull_request.user.login
+    })); 
+    const countPR = response.data.filter(data => data.pull_request); 
+    try {
+      let repo = context.repo(); 
+      context.github.issues.createComment(context.issue( {
+        body:"Hello World!"
+      })); 
+    }catch (err) {
+      if (err.code !== 404) {
+        throw err; 
+      }
+    }
+  }
+}; 
